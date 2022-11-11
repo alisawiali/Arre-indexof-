dc6cfa45faed663c2627a035f364922b894f8266
iimad <alisawiali70@gmail.com>
2022-11-11 09:47:25 +0100

    indexOf mit arr

---
+++ main.js
@@ -0,0 +1,15 @@
+let arr =['Ali', 'Imad', 'Alisawi','Gmal','Omer']
+
+// console.log(arr.includes('Ali')) treu oder falsch mit includes....
+
+// console.log(arr.indexOf('Alisawi',0))...suche nach dem name mit zahl
+
+/*
+console.log(arr.sort())
+
+let arr2 =[1,2,33,4,5];
+console.log(arr2.sort())
+
+arr2.splice(2,0, "Almasuri")
+console.log(arr2)*/
+
